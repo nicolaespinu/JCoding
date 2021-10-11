64571520e23d1d7be81fe8f00cb60d771811bf7f
Nicolae Spinu <11spinic@gmail.com>
2021-10-11 11:51:02 +0300

    LC Maximum subarray

--- src/main/java/leetcode/MaximumSubarray.java
+++ src/main/java/leetcode/MaximumSubarray.java
@@ -1,24 +1,14 @@
 package leetcode;
 
 public class MaximumSubarray {
-    public static void main(String[] args) {
-//        int [] nums = {-2,1,-3,4,-1,2,1,-5,4};  // 6
-//        int [] nums = {5,4,-1,7,8};  // 23
-//        int [] nums = {1};  // 1
-//        int[] nums = {-2, 1};  // 1
-    }
 
-    public static int maxSubArray(int[] nums) {
-        int result = 0, max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                result += nums[j];
-                if (result > max) {
-                    max = result;
-                }
-            }
-            result = 0;
+    public static int maxSubArray(int[] ints) {
+        int maxSumm = ints[0], summ = 0;
+        for (int i = 0; i < ints.length; i++) {
+            summ = Math.max(summ, 0);
+            summ += ints[i];
+            maxSumm = Math.max(maxSumm, summ);
         }
-        return max;
+        return maxSumm;
     }
 }
