4a14b38ca61a51a0b6baa29135444b26be3586cb
Nicolae Spinu <11spinic@gmail.com>
2021-12-17 16:56:38 -0500

    LC remove dublicates from Sorted list

---
+++ src/main/java/leetcode/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+public class RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(2);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(3);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        System.out.println();
+        showList(node1);
+        System.out.println();
+        deleteDuplicates(node1);
+        System.out.println("Deleted");
+        showList(node1);
+
+    }
+
+    public static void showList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        while (node != null && node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return node;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
